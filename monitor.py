7a060529cd17ebd8bb0ae56d46fca300fa59bd90
Rgivens14 <rgivens14@gmail.com>
2026-09-08 12:36:54 -0400

    The monitor now runs the simulator and displays the output. I still need to extract the data from the script

--- monitor.py
+++ monitor.py
@@ -1,2 +1,15 @@
 import subprocess #use .Popen to get instant output
 import json #
+
+#you will need to create a monitor. Use the subproccess to run the simulator
+#make sure that the compile output is converted to its text form
+
+process = subprocess.Popen(
+    ["./telemetry_sim"],
+    stdout=subprocess.PIPE,
+    text=True
+)
+
+for line in process.stdout:
+    line = line.strip()
+    print("Received raw line:", line) # will edit this later to only use the line value
