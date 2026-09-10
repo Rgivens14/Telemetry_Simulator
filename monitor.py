d114e49583bb341b3a8255cc9a618fea762a3b42
Rgivens14 <rgivens14@gmail.com>
2026-09-10 07:43:20 -0400

    The monitor now interprets the json data. Setting up values and color codes for when the indicators exceed a specified range

--- monitor.py
+++ monitor.py
@@ -10,6 +10,25 @@ process = subprocess.Popen(
     text=True
 )
 
+MAX_TEMP = 85.0
+MAX_VIBRATION = 75.0
+MAX_VOLTAGE = 125.25
+
+RED = "\033[91m"
+GREEN = "\033[92m"
+RESET = "\033[0m"
+
+print(f"{GREEN}[OK]{RESET} System normal")
+print(f"{RED}[ALERT]{RESET} High temperature detected!")
+
 for line in process.stdout:
     line = line.strip()
-    print("Received raw line:", line) # will edit this later to only use the line value
+    #print("Received raw line:", line)
+
+    try:
+        json_data = json.loads(line)
+        temp = json_data["temperature_c"]
+        print(temp)
+
+    except (json.JSONDecodeError, KeyError) as e:
+        print(f"Error processing telemetry line: {e}")
