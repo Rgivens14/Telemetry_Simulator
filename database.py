330c9ec6dfd9bef86662a04ebddd912990de930b
Rgivens14 <rgivens14@gmail.com>
2026-09-18 17:09:59 -0400

    Placeholder commit. The database still needs to have more configuration before testing.

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 import _sqlite3
 
-conn = _sqlite3.connect("telemetry_database.db")
+conn = _sqlite3.connect("telemetry_database2.db")
 
 cur = conn.cursor()
 cur.execute("CREATE TABLE device(device_id, temperature_c, voltage_kv, optical_loss_db, vibration_g)")
+
+res = cur.execute("SELECT name FROM sqlite_master")
+res.fetchone()
